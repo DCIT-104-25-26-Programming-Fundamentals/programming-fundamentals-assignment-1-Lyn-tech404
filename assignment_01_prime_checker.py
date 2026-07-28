9b2ac7b8488214dcc7c6bdb6071a357d95b14d7a
Lyn-tech404 <obenglyn13@gmail.com>
2026-07-28 14:34:39 -0700

    Define is_prime function for prime checking

--- assignment_01_prime_checker.py
+++ assignment_01_prime_checker.py
@@ -33,7 +33,8 @@
 
 # =============================================================================
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
-# =============================================================================def is_prime(number):
+# =============================================================================
+def is_prime(number):
     if number < 2:
         return False
     for i in range(2, number):
